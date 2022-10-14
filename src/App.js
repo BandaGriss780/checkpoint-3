0ff5a5c1eb73a284b2cab3cb3cb7cd63db71e49c
dfma-89 <dfma.mdp@gmail.com>
2022-10-14 10:26:30 -0300

    FAVICON+FOOTER

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BooksList from "./components/BooksList";
 import BookForm from "./components/BookForm";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-zinc-600  h-screen text-white">
       <NavBar />
       <div className="flex items-center justify-center">
-        <BrowserRouter>
           <Routes>
             <Route path="/" element={<BooksList />} />
             <Route path="/create-book" element={<BookForm />} />
             <Route path="/edit-book/:id" element={<BookForm />} />
           </Routes>
-        </BrowserRouter>
+      <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    const year = new Date().getFullYear();
+  
+    return (
+        <div>
+        <footer>
+            {`Created by: Ian, Mati, Gise & Debo -
+            Checkpoint 3 Avalith®${year}`}
+        </footer>
+        </div>
+)};
+  
+  export default Footer;
